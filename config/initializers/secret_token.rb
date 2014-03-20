c8d63c236ca68633b272ee411ce18199a7b30e6a
hxx <845059331@qq.com>
2014-03-20 22:30:10 +0800

    Initial and add the article modle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'fd1b00277201de32decaa8d095e349c0e37f3573ad7dc0ded1406581ab461886072cbd615a5a99fc0ba212cf2d8b0b8a7782a5600c8bb8d7921f755a229e442d'
