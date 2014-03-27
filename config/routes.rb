96e80f38f255358778d7c9fb40a2131314f5ad2c
hxx <845059331@qq.com>
2014-03-27 10:48:10 +0800

    Add the sessions to authenticate user
    add user model,session model, sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Blog::Application.routes.draw do
   resources :articles do
     resources :comments
   end
+
+  resources :sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
