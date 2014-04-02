c12b7f61815f41e8080ec43d406478e96993c563
hxx <845059331@qq.com>
2014-04-02 22:17:36 +0800

    Add the tag for article
    add the model tag and tagging
    use the tagging for article and tag through has_many
    add the tag_with find the article by tag

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,14 @@ class Article < ActiveRecord::Base
   has_many  :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+
+  has_many :taggings
+  has_many :tags, :through => :taggings
+
+  scope :tag_with, lambda{ |tag_name| joins(:tags).where("tags.name = ?", tag_name) }
+  scope :latter_than, lambda{ |time| joins(:taggings).where("taggings.created_at > ?", time) }
+
+  # def self.tag_with(tag_name)
+  #   Tag.find_by(name: tag_name).try(:articles)
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140327021826) do
+ActiveRecord::Schema.define(version: 20140402132914) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -29,6 +29,19 @@ ActiveRecord::Schema.define(version: 20140327021826) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "taggings", force: true do |t|
+    t.integer  "article_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "login"
